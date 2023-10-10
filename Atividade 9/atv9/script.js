aab217a21aeebed77a710f5560072dfcfd5c65d1
Julia Rodrigues <105590655+RJjulia@users.noreply.github.com>
2023-10-09 22:06:09 -0300

    Add files via upload

---
+++ Atividade 9/atv9/script.js
@@ -0,0 +1,54 @@
+var entrevistados = new Array(45);
+var i;
+
+totalIdade = 0;
+idadeMaisNova = 120;
+qtdPessimo=0; 
+qtdBom = 0;
+qtdOtimo = 0;
+mulheres = 0;
+homens = 0;
+idadeMaisVelha = 0;
+
+for(i=0; i<entrevistados.length; i++){
+    idade = parseInt(prompt("Idade: "));
+    sexo = prompt ("Sexo (F - feminino e M - Masculino): ");
+    opniao = parseFloat(prompt("Opinião (ótimo=4, bom=3, regular=2, péssimo=1): "));
+
+    
+    totalIdade += idade;
+    
+
+    if (idadeMaisVelha < idade)
+        idadeMaisVelha = idade;
+   
+    
+    if (idadeMaisNova > idade)
+        idadeMaisNova = idade;
+    
+    
+    if(opniao === 1)
+        qtdPessimo++;
+    
+    if(opniao === 3)
+        qtdBom++;
+    
+    if(opniao === 4)
+        qtdOtimo++;
+
+    if(sexo.toUpperCase() === "F")
+        mulheres++;
+    else if (sexo.toUpperCase() === "M")
+        homens++;
+}
+
+media = totalIdade / entrevistados.length;
+
+alert("Média da idade das pessoas que responderam ao questionário: " + media + " anos.");
+alert("Idade da pessoa mais velha: " + idadeMaisVelha + " anos.");
+alert("Idade da pessoa mais nova: " + idadeMaisNova + " anos.");
+alert("Quantidade de pessoas que responderam Péssimo: " + qtdPessimo);
+alert("Porcentagem de pessoas que responderam Ótimo: " + ((qtdOtimo*100)/entrevistados.length).toFixed(2) + "%.");
+alert("Porcentagem de pessoas que responderam Bom: " + ((qtdBbom*100)/entrevistados.length).toFixed(2) + "%.");
+alert("Número de mulheres que respoderam ao questionário: " + mulheres + ".");
+alert("Número de homens que respoderam ao questionário: " + homens + ".");
